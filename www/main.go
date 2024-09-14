52b41d4815e534d14709da3af8edb339a2af0b11
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2024-09-14 17:09:50 +0900

    Add: sample

---
+++ www/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// 静的ファイルのディレクトリを指定（例: ./static）
+	fileServer := http.FileServer(http.Dir("./"))
+
+	// ルートパスにファイルサーバーを割り当てる
+	http.Handle("/", fileServer)
+
+	port := ":5151"
+	fmt.Printf("サーバーがポート%sで起動しています...\n", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		fmt.Printf("サーバーエラー: %v\n", err)
+	}
+}
