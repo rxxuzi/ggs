6efc7bfc70d28c267dea5a9b6bc94d19ce20ff27
toyama <tyksi0724@gmail.com>
2024-09-14 19:08:16 +0900

    joinボタンでcommunityリンクに遷移するようにしました

--- www/app.js
+++ www/app.js
@@ -45,16 +45,20 @@ function showEventDetail(eventId) {
         <div class="modal-content">
             <span class="close">&times;</span>
             <h2>${event.eventName}</h2>
-            <p>${event.description}</p>
             <p><img src="./img/fas/calender.svg" alt="Calendar"> 締切: ${event.deadline}</p>
             <p><img src="./img/fas/user.svg" alt="User"> 応募条件: ${event.conditions}</p>
             <p>
                 <img src="${getPlatformIconPath(event.platform)}" alt="${event.platform} icon" class="event-icon">
                 プラットフォーム: ${event.platform}
             </p>
-            <p><img src="./img/fas/person.svg" alt="author"> 主催者: ${event.author}</p>
             <button class="button join-button"><img src="./img/fas/user.svg" alt="User"> Join</button>
-            <button class="button share-button"><img src="./img/fas/share.svg" alt="Share"> Share</button>
+            <div class="dropdown">
+                <button class="button share-button"><img src="./img/fas/share.svg" alt="Share"> Share</button>
+                <div class="dropdown-content">
+                    <button class="button copy-link-button">Copy link</button>
+                    <button class="button share-x-button">Share on X</button>
+                </div>
+            </div>
             <button class="button bookmark-button" data-id="${event.id}">
                 <img src="${bookmarks[event.id] ? './img/fas/bm-ed.svg' : './img/fas/bm.svg'}" alt="Bookmark">
                 ${bookmarks[event.id] ? 'ブックマーク解除' : 'ブックマーク'}
@@ -63,13 +67,28 @@ function showEventDetail(eventId) {
     `;
     modal.style.display = "block";
 
+    // 閉じるボタン
     modal.querySelector('.close').addEventListener('click', () => {
         modal.style.display = "none";
     });
 
-    // [join]ボタンが押されたときにcommunityLinkに遷移
+    // [Join]ボタンが押されたときにcommunityLinkに遷移
     modal.querySelector('.join-button').addEventListener('click', () => {
         window.open(event.communityLink, '_blank'); // 新しいタブで開く
+    });
+
+    // [Copy link] ボタンでリンクをクリップボードにコピー
+    modal.querySelector('.copy-link-button').addEventListener('click', () => {
+        navigator.clipboard.writeText(event.communityLink)
+            .then(() => alert("リンクがクリップボードにコピーされました！"))
+            .catch(err => console.error('クリップボードへのコピーに失敗しました:', err));
+    });
+
+    // [Share on X] ボタンでXにシェア
+    modal.querySelector('.share-x-button').addEventListener('click', () => {
+        const shareText = `イベント: ${event.eventName}\n締切: ${event.deadline}\nリンク: ${event.communityLink}`;
+        const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(shareText)}`;
+        window.open(twitterUrl, '_blank');
     });
 
     modal.querySelector('.bookmark-button').addEventListener('click', (e) => {
@@ -83,6 +102,7 @@ function showEventDetail(eventId) {
         }
     });
 }
+
 
 
 function displayEventList() {
