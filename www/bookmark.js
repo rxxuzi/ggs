7a3c73d9c6c7f058d8d6b717ae2f6bb8ce9817da
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2024-09-15 09:19:48 +0900

    update bookmark

--- www/bookmark.js
+++ www/bookmark.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     waitForEventsData().then(() => {
         displayBookmarkedEvents();
-        setupSearchFunctionality();
-        setupCollapsibles();
     });
 });
+
 function waitForEventsData() {
     return new Promise((resolve) => {
         const checkEventsData = () => {
@@ -16,42 +15,6 @@ function waitForEventsData() {
         };
         checkEventsData();
     });
-}
-
-function setupSearchFunctionality() {
-    const searchInput = document.getElementById('search-input');
-    const searchButton = document.getElementById('search-button');
-
-    if (searchInput && searchButton) {
-        searchButton.addEventListener('click', () => performSearch(searchInput.value));
-        searchInput.addEventListener('keypress', (e) => {
-            if (e.key === 'Enter') {
-                performSearch(searchInput.value);
-            }
-        });
-    } else {
-        console.error('Search input or button not found');
-    }
-}
-
-function performSearch(query) {
-    const searchResults = eventsData.filter(event =>
-        event.eventName.toLowerCase().includes(query.toLowerCase()) ||
-        event.shortDescription.toLowerCase().includes(query.toLowerCase())
-    );
-
-    const searchResultsContainer = document.getElementById('search-results');
-
-    if (searchResultsContainer) {
-        if (searchResults.length === 0) {
-            searchResultsContainer.innerHTML = '<p>検索結果が見つかりませんでした。</p>';
-        } else {
-            searchResultsContainer.innerHTML = searchResults.map(createEventHTML).join('');
-            addEventListeners(searchResultsContainer);
-        }
-    } else {
-        console.error('Search results container not found');
-    }
 }
 
 function displayBookmarkedEvents() {
@@ -71,22 +34,6 @@ function displayBookmarkedEvents() {
         bookmarkedEventsContainer.innerHTML = bookmarkedEvents.map(createEventHTML).join('');
         addEventListeners(bookmarkedEventsContainer);
     }
-}
-
-function setupCollapsibles() {
-    const collapsibles = document.querySelectorAll('.collapsible');
-
-    collapsibles.forEach(collapsible => {
-        collapsible.addEventListener('click', function() {
-            this.classList.toggle('active');
-            const content = this.nextElementSibling;
-            if (content.style.maxHeight) {
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-            }
-        });
-    });
 }
 
 function createEventHTML(event) {
@@ -111,7 +58,6 @@ function createEventHTML(event) {
     `;
 }
 
-// addEventListeners 関数は app.js から移動
 function addEventListeners(container) {
     container.querySelectorAll('.event-item').forEach(item => {
         item.addEventListener('click', (e) => {
